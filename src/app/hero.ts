0033107dac07f5075be04b4de31e21300ffcd962
devin <bowen61496@gmail.com>
2024-11-09 22:38:33 -0500

    Adding pipes

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,14 +1,3 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
-
-@NgModule({
-  imports: [
-    CommonModule, 
-    // other modules
-  ],
-  // ... rest of your module 
-})
-export class YourModule { } 
 
 export interface Hero {
     id: number;
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
